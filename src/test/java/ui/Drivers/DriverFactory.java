da01c4eea7730ce6050aeaef78d41091af5fe779
Darya Huzich <guzich.daria@gmail.com>
2025-08-24 14:48:44 +0200

    Updated DriverFactory

--- src/test/java/ui/Drivers/DriverFactory.java
+++ src/test/java/ui/Drivers/DriverFactory.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
+import java.util.UUID;
 
 public class DriverFactory {
     private static WebDriver webDriver;
@@ -30,7 +31,28 @@ public class DriverFactory {
                 default:
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
+
+                    // Основные аргументы для Docker/headless режима
                     options.addArguments("--headless");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    options.addArguments("--disable-gpu");
+                    options.addArguments("--remote-debugging-port=0");
+                    options.addArguments("--disable-web-security");
+                    options.addArguments("--disable-features=VizDisplayCompositor");
+
+                    // Уникальная директория пользователя для каждого экземпляра
+                    options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + UUID.randomUUID().toString());
+
+                    // Дополнительные стабилизирующие аргументы
+                    options.addArguments("--disable-extensions");
+                    options.addArguments("--disable-plugins");
+                    options.addArguments("--disable-images");
+                    options.addArguments("--disable-javascript");
+                    options.addArguments("--disable-background-timer-throttling");
+                    options.addArguments("--disable-backgrounding-occluded-windows");
+                    options.addArguments("--disable-renderer-backgrounding");
+
                     webDriver = new ChromeDriver(options);
             }
             webDriver.manage().window().maximize();
