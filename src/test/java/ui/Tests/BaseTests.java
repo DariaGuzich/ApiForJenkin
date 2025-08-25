4e980296f0e3bcce23692b89d49e18c42e372f8e
Darya Huzich <guzich.daria@gmail.com>
2025-08-25 16:09:09 +0200

    Added logging functionality to BaseTests and LoginTests; configured logback for logging output

--- src/test/java/ui/Tests/BaseTests.java
+++ src/test/java/ui/Tests/BaseTests.java
@@ -1,9 +1,10 @@
 package ui.Tests;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ui.Drivers.DriverFactory;
 import ui.Drivers.DriverTypes;
 import ui.Utils.TestResultExtension;
@@ -11,9 +12,13 @@ import ui.Utils.TestResultExtension;
 @ExtendWith(TestResultExtension.class)
 public class BaseTests {
     protected WebDriver webDriver;
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTests.class);
 
     @BeforeEach
     public void initialize() {
+        logger.info("Инициализация теста...");
+        logger.info("Создание экземпляра WebDriver (Chrome)");
         webDriver = DriverFactory.getDriver(DriverTypes.Chrome);
+        logger.info("WebDriver успешно инициализирован");
     }
 }
--- src/test/java/ui/Tests/LoginTests.java
+++ src/test/java/ui/Tests/LoginTests.java
@@ -2,14 +2,19 @@ package ui.Tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ui.PageObjects.LoginPage;
 import ui.PageObjects.MainPage;
 
 public class LoginTests extends BaseTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginTests.class);
+
     @Test
     public void validLoginTest() {
         LoginPage loginPage = new LoginPage(webDriver);
+        logger.info("Выполнение входа с учётными данными: Admin / admin123");
         loginPage.login("Admin", "admin123");
         MainPage mainPage = new MainPage(webDriver);
         Assertions.assertTrue(mainPage.isDropDownProfileDisplayed(), "Main page wasn't properly opened!");
@@ -18,6 +23,7 @@ public class LoginTests extends BaseTests {
     @Test
     public void invalidLoginTest() {
         LoginPage loginPage = new LoginPage(webDriver);
+        logger.info("Выполнение входа с учётными данными: wrongName / wrongPassword");
         loginPage.login("wrongName", "wrongPassword");
         Assertions.assertTrue(loginPage.isErrorMessageDisplayed(), "Error message is expected to be displayed!");
     }
@@ -25,6 +31,7 @@ public class LoginTests extends BaseTests {
     @Test
     public void invalidLoginTest_MustFail() {
         LoginPage loginPage = new LoginPage(webDriver);
+        logger.info("Выполнение входа с учётными данными: wrongName / wrongPassword");
         loginPage.login("wrongName", "wrongPassword");
         Assertions.assertFalse(loginPage.isErrorMessageDisplayed(), "Error message is expected to be displayed!");
     }
