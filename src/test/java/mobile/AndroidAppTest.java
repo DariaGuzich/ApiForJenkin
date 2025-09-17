1285710f3796f103ea4d5c2434960df2fe99946a
Darya Huzich <guzich.daria@gmail.com>
2025-09-17 16:06:40 +0200

    Refactor AndroidAppTest to retrieve UDID from system properties and clean up docker-compose configuration

--- src/test/java/mobile/AndroidAppTest.java
+++ src/test/java/mobile/AndroidAppTest.java
@@ -17,9 +17,9 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AndroidAppTest {
-    
+
     private AndroidDriver driver;
-    
+
     @BeforeEach
     public void setUp() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
@@ -29,10 +29,11 @@ public class AndroidAppTest {
         String deviceName = System.getProperty("device.name", "android-emulator");
         options.setDeviceName(deviceName);
 
-        // Настройки для подключения к удаленному устройству
-        String deviceHost = System.getProperty("device.host", "android-emulator");
-        String devicePort = System.getProperty("device.port", "5555");
-        options.setCapability("appium:udid", deviceHost + ":" + devicePort);
+        // Для реального устройства - получаем UDID из adb devices
+        String udid = System.getProperty("device.udid", ""); // Реальный UDID устройства
+        if (!udid.isEmpty()) {
+            options.setCapability("appium:udid", udid);
+        }
 
         options.setPlatformVersion("13");
         options.setAutomationName("UiAutomator2");
@@ -48,51 +49,51 @@ public class AndroidAppTest {
         // Настройки для работы с удаленным устройством
         options.setCapability("appium:connectHardwareKeyboard", true);
         options.setCapability("appium:systemPort", 8200);
-        
+
         String appiumHost = System.getProperty("appium.host", "localhost");
         String appiumPort = System.getProperty("appium.port", "4723");
         driver = new AndroidDriver(new URL("http://" + appiumHost + ":" + appiumPort), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
-    
+
     @Test
     public void testHomeScreenAndOpenApp() throws InterruptedException {
         System.out.println("Начинаем тест домашнего экрана Android...");
-        
+
         // Проверяем, что домашний экран загрузился
         WebElement homeScreen = driver.findElement(By.xpath("//*[@content-desc='Home']"));
         assertTrue(homeScreen.isDisplayed(), "Домашний экран должен быть виден");
         System.out.println("Домашний экран успешно загружен!");
-        
+
         // Проверим наличие иконки Phone
         WebElement phoneApp = driver.findElement(By.xpath("//*[@text='Phone']"));
         assertTrue(phoneApp.isDisplayed(), "Приложение Phone должно быть видно");
         System.out.println("Приложение Phone найдено!");
-        
+
         // Кликаем на Phone
         phoneApp.click();
         Thread.sleep(3000);
         System.out.println("Приложение Phone успешно открылось!");
-        
+
         // Возвращаемся на домашний экран
         driver.pressKey(new KeyEvent(AndroidKey.HOME));
         Thread.sleep(1000);
         System.out.println("Вернулись на домашний экран");
     }
-    
-    
+
+
     @AfterEach
     public void tearDown() throws InterruptedException {
         if (driver != null) {
             // Принудительно возвращаемся на домашний экран
             driver.pressKey(new KeyEvent(AndroidKey.HOME));
             Thread.sleep(500);
-            
+
             // Закрываем все фоновые приложения
             driver.pressKey(new KeyEvent(AndroidKey.APP_SWITCH));
             Thread.sleep(1000);
             driver.pressKey(new KeyEvent(AndroidKey.HOME));
-            
+
             driver.quit();
         }
     }
